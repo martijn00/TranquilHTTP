2dd1b6255591470ef4d8f3b0f98b0f0a2b29a40f
Maurits van Beusekom <m.v.van.beusekom@isala.nl>
2016-04-06 14:57:12 +0200

    Updated project information

--- Tranquil HTTP/Properties/AssemblyInfo.cs
+++ Tranquil HTTP/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tranquil HTTP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tranquil HTTP is a C# HTTP library for Mobile Development aiming to make it really easy to solve complex HTTP communication scenarios.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Route2IT")]
 [assembly: AssemblyProduct("Tranquil HTTP")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
